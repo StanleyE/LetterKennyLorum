69c565bbe03b6fa0789a6a3c22eda81fc1565774
Erica <Erica.Stanley@live.ca>
2018-07-14 14:42:54 -0400

    finished adding safe for work data

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,8 @@ db.on('open', ()=>{
 
 const Quote = require('./models/Quote');
 
-let data = ['Who Garrett? No, Jarrett.', 'Hi Wade.', 'How are you now?' , 'Boots and the Ginger.', 'Not so bad.', 'Texas size 10-4.', 'Toughest guy in Letterkenny.', 'Let’s go easy over there, Squirrel Dan.', 'Back to Choring.'];
-// Add new Quote
+let data = ['Pitter Patter.', 'Fart Book.', 'I guess we gots to calls up the internet.', 'Maybe if you’d ever been in a fight, you might not be so keen to be in one.', 'Super Soft birthday party.', 'Pump the brakes.', 'You guys do CrossFit?', 'Wayne look, ….It’s inappropriate McMurry.', 'You’re really gonna pump the brakes on a donkey dressed up as a unicorn?', 'Well here’s a tip, don’t fart in a space suit.', 'Figure it out.', 'Not my pig, not my farm.', 'Oh get off the cross, we need the wood.', 'You know, not to be impolite…', 'Oh, it’s too nice a day to get cross.', 'Go Time!', 'Great fishing in Quebec.', 'Oh…. that is noisy Dan.', 'Oh, that’s near Doctor Assisted Dan.', 'You better settle down over there, else I’m gonna come talk to ya.', 'It’s impolite to kiss and tell.', 'When the cat’s away, the mice will play.'];
+// Add new Quote Array
 function dataEntry(data) {
     const Quote = require('./models/Quote');
     for (let i = 0; i < data.length; i++) {
@@ -27,9 +27,10 @@ function dataEntry(data) {
             });
     }
 }
-// console.log(dataEntry(data));
-// console.log(data[0]);
-dataEntry(data);
+
+//dataEntry(data);
+
+//Add new quote singular
 
 // let newQ = Quote({
 //     quote: '',
