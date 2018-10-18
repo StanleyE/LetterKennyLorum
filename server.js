0f219f081cd7ed15faec767407cbdbb62d0d44a5
Erica <Erica.Stanley@live.ca>
2018-10-18 14:55:05 -0400

    adding everything to see if itll work

--- server.js
+++ server.js
@@ -121,13 +121,13 @@ app.get('*', (req, res) => {
     })
     
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(__dirname + './client/build'));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + './client/build'));
 
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     })
-// }
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 // express listener
 app.listen(PORT, () => {
