531f3c5b1e252730ec792ca4b3f413b23ec8e394
Erica <Erica.Stanley@live.ca>
2018-10-18 13:13:57 -0400

    ugh! why wont this work!?

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use(express.static(__dirname + './client/build'));
+app.use(express.static('./client/build'));
 
 
 const PORT = process.env.PORT || 8080;
